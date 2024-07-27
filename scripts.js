a654c154a1ef4beda32fe58eeece08e1573e7567
Phearum1111 <phearumcv@gmail.com>
2024-07-27 21:18:24 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const factButton = document.getElementById("fact-button");
+    factButton.addEventListener("click", function() {
+        alert("Did you know? Trees can communicate with each other through underground networks of fungi.");
+    });
+});
